ee621d2f254667740c327b2fc48cd041b66daa03
nemeros <nemeros@github.com>
2016-02-25 15:12:30 +0100

    logout

--- StaticApp/src/main/webapp/scripts/components/navbar/NavBarController.js
+++ StaticApp/src/main/webapp/scripts/components/navbar/NavBarController.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('myApp')
-    .controller('NavbarController', function ($scope, $log) {
-
+    .controller('NavbarController', function ($scope, $http, $localStorage, $log) {
+    	$scope.logout = function(){
+    		delete $localStorage.jwtAuth;
+    		$http.defaults.headers.common['X-AUTH-TOKEN'] = "";
+    	};
     });
